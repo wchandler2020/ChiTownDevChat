c267cf09a62c90e6af954c600af6d5518295cc53
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-04-18 13:58:09 -0400

    added animaion to homescreen and Login activity

---
+++ app/src/main/java/com/example/chitowndevchat/Activities/LoginPage.java
@@ -0,0 +1,16 @@
+package com.example.chitowndevchat.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chitowndevchat.R;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
--- app/src/main/java/com/example/chitowndevchat/MainActivity.java
+++ app/src/main/java/com/example/chitowndevchat/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.chitowndevchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.chitowndevchat.Activities.LoginPage;
 
 public class MainActivity extends AppCompatActivity {
 
     //for xml views
-
+    Button moveToLoginPageBtn;
+    ImageView backgroundIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        attachJavaObjectXML();
+        startAnimations();
+
+        moveToLoginPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLoginPage();
+            }
+        });
+    }
+
+    private  void attachJavaObjectXML()
+    {
+        try {
+            backgroundIV=findViewById(R.id.mainActivity_BackgroundIV);
+            moveToLoginPageBtn=findViewById(R.id.mainActivity_MoveBtn);
+        }catch (Exception e)
+        {
+            Toast.makeText(this, "MainAction: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startAnimations()
+    {
+        try
+        {
+            backgroundIV.animate().scaleX(2).scaleY(2).setDuration(30000).start();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "MainActivity: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void goToLoginPage()
+    {
+        try
+        {
+            startActivity(new Intent(this, LoginPage.class));
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "MainActivity" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
