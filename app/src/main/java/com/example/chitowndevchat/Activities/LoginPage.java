b1f2acbbbd14430eb297d3ccfbe2d895d215f3c6
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-04-18 20:41:28 -0400

    added animatino to login screen

--- app/src/main/java/com/example/chitowndevchat/Activities/LoginPage.java
+++ app/src/main/java/com/example/chitowndevchat/Activities/LoginPage.java
@@ -2,15 +2,45 @@ package com.example.chitowndevchat.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.chitowndevchat.R;
 
 public class LoginPage extends AppCompatActivity {
 
+    //Vars for XML
+    RelativeLayout objectRelativeLayout;
+
+    // Class Vars
+    private AnimationDrawable objectAnimationDrawable;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        attachJavaObjectToXML();
+    }
+
+    private void attachJavaObjectToXML()
+    {
+        try
+        {
+            objectRelativeLayout = findViewById(R.id.loginPage_RL);
+            objectAnimationDrawable=(AnimationDrawable) objectRelativeLayout.getBackground();
+
+            objectAnimationDrawable.setEnterFadeDuration(4500);
+            objectAnimationDrawable.setExitFadeDuration(4500);
+
+            objectAnimationDrawable.start();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "LoginPage: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
